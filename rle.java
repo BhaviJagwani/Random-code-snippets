c38fd1e7cf665f0764cb2f3068ade36763ba29bc
Bhavi Jagwani <jagwani.bhavi@gmail.com>
2011-09-10 22:52:02 +0530

    Edited rle.java via GitHub

--- rle.java
+++ rle.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class bhavi {
+class rle {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter a string of characters");
@@ -8,18 +8,18 @@ class bhavi {
 		int len=str.length();
 		System.out.print("$");
 		for(int i=0; i<len-1; i++)                      
-		  { if(str.charAt(i)==str.charAt(i+1))  
-		     count++;
+		   {if(str.charAt(i)==str.charAt(i+1))  
+		       count++;
 		    else if (count==1) 
-                                              {System.out.print(str.charAt(i));}
+                            {System.out.print(str.charAt(i));}
 		          else if ( count>1)
-                                                   { System.out.print(count);
-                                                    System.out.print(str.charAt(i));   
+                                  { System.out.print(count);
+                                    System.out.print(str.charAt(i));   
 		                    count=1;}
-                                   }
-		   if(count>1)                                              
-		   System.out.print(count);
-		   System.out.print(str.charAt(len-1));
+                    }
+		 if(count>1)                                              
+		 System.out.print(count);
+		 System.out.print(str.charAt(len-1));
 	}
 
 }
